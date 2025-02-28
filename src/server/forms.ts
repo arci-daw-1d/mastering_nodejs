3417804d012dc261078aaa1be0e9e6ef50f27aaa
arci-daw-1d <arcipreste.daw.programacion@gmail.co>
2025-02-28 19:57:18 +0100

    Fin Cap 12

--- src/server/forms.ts
+++ src/server/forms.ts
@@ -1,33 +1,29 @@
 import express, { Express } from 'express';
-// import multer from "multer";
-// import { santizeValue } from "./sanitize";
-import { getValidationResults, validate} from "./validation";
+import repository  from "./data";
 
-// const fileMiddleware = multer({ storage: multer.memoryStorage() });
+const rowLimit = 10;
 
 export const registerFormMiddleware = (app: Express) => {
     app.use(express.urlencoded({ extended: true }));
 };
 
 export const registerFormRoutes = (app: Express) => {
-    app.get("/form", (req,  res ) => {
-        res.render("age", { helpers: { pass }});
+    app.get("/form", async (req, resp) => {
+        resp.render("age", {
+            history: await repository.getAllResults(rowLimit)
+        });
     });
 
-    app.post("/form",
-        validate("name").required().minLength(5),
-        validate("age").isInteger(),
-        (req, res) => {
-            const validation = getValidationResults(req);
-            const context = {...req.body, validation, helpers: { pass }};
-            if (validation.valid){
-                context.nextage = Number.parseInt(req.body.age) + 1;
-            }
-            res.render("age", context);
+    app.post("/form", async (req, resp) => {
+        const nextage = Number.parseInt(req.body.age)
+            + Number.parseInt(req.body.years);
+
+        await repository.saveResult({...req.body, nextage});
+
+        const context = {
+            ...req.body, nextage,
+            history: await repository.getResultsByName( req.body.name,rowLimit)
+        };
+        resp.render("age", context);
     });
 };
-
-const pass = (valid: any, propname: string, test: string ) => {
-    let propResult = valid?.results?.[propname];
-    return `display:${!propResult || propResult[test] ? "none" : "block" }`;
-}
--- src/server/server.ts
+++ src/server/server.ts
@@ -1,6 +1,5 @@
 import { createServer } from "node:http";
 import express, { Express} from "express";
-import { testHandler } from "./testHandler";
 import httpProxy from "http-proxy";
 import helmet from "helmet";
 import { engine } from "express-handlebars";
@@ -15,23 +14,6 @@ const proxy = httpProxy.createProxyServer({
     target: "http://localhost:5100", ws: true,
 });
 
-
-// expressApp.use(helmet({
-//     contentSecurityPolicy: {
-//         directives: {
-//             imgSrc: "self",
-//             scriptSrc: ['self', 'unsafe-eval', 'unsafe-inline'],
-//             // scriptSrcAlt: "none",
-//             connectSrc: "'self' ws://localhost:5000",
-//         }
-//     }
-// }));
-//    <meta http-equiv="Content-Security-Policy"
-//    content="script-src 'self' 'unsafe-eval' 'unsafe-inline';
-//    object-src 'self';
-//    style-src 'self' 'unsafe-inline';
-//    media-src *">
-
 expressApp.set("views", "templates/server");
 expressApp.engine("handlebars", engine());
 expressApp.set("view engine", "handlebars");
@@ -42,16 +24,8 @@ expressApp.use(express.json());
 registerFormMiddleware(expressApp);
 registerFormRoutes(expressApp);
 
-expressApp.get("/dynamic/:file", (req: express.Request, res: express.Response) => {
-    res.render(`${req.params.file}.handlebars`,
-        {
-            message:"Hello template",
-            req,
-            helpers: {...helpers}
-        });
-});
+expressApp.use("^/$", (req, resp) => { resp.redirect("/form")});
 
-expressApp.post("/test", testHandler);
 expressApp.use(express.static("static"));
 expressApp.use(express.static("node_modules/bootstrap/dist/"));
 expressApp.use(express.static("dist/client"));
